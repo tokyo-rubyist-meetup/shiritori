7bc0f816b03415c5009bcb26af0a3dea86d3c3ce
Paul McMahon <paul@doorkeeper.jp>
2018-10-21 17:55:54 +0900

    Extract list of Japanese words to helper
    Probably will be needed if you want to do TDD, and might not be obvious
    how to do to beginners.

--- bin/shiritori.rb
+++ bin/shiritori.rb
@@ -3,15 +3,13 @@
 $:.unshift File.expand_path("../lib", __dir__)
 require "shiritori"
 
-word_data_file = File.expand_path('../data/word_data.txt', __dir__)
-japanese_nouns = File.readlines(word_data_file).map {|string| string.chomp! }
-japanese_nouns_as_katakana = japanese_nouns.map {|string| Shiritori.to_katakana(string) }
+japanese_nouns_as_katakana = Shiritori.japanese_nouns.map {|string| Shiritori.to_katakana(string) }
 
 puts "I'll let you start. What's your first word?"
 loop do
   word = Shiritori.to_katakana(gets.chomp)
   if japanese_nouns_as_katakana.include?(word)
-    puts japanese_nouns.sample
+    puts Shiritori.japanese_nouns.sample
   else
     puts "That doesn't count! You lose."
     exit
--- lib/shiritori.rb
+++ lib/shiritori.rb
@@ -4,4 +4,11 @@ class Shiritori
   def self.to_katakana(string)
     NKF.nkf("-w -h2", string)
   end
+
+  def self.japanese_nouns
+    @japanese_nouns ||= begin
+      word_data_file = File.expand_path('../data/word_data.txt', __dir__)
+      File.readlines(word_data_file).map {|string| string.chomp! }
+    end
+  end
 end
